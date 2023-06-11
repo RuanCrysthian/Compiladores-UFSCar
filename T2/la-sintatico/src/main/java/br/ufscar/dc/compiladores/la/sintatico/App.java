88531106e26fd4b9f05720bd4a9295eb48c663db
Ruan Crysthian <ruanlima155@gmail.com>
2023-06-11 14:07:00 -0300

    configuracao inicial

---
+++ T2/la-sintatico/src/main/java/br/ufscar/dc/compiladores/la/sintatico/App.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.compiladores.la.sintatico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
