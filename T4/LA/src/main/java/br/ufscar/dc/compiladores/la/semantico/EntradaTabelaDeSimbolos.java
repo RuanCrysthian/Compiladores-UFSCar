d15af02479d84f9e403fcd71a596346ce53f9601
RuanCrysthian <ruanlima155@gmail.com>
2023-08-08 19:23:20 -0300

    testye

---
+++ T4/LA/src/main/java/br/ufscar/dc/compiladores/la/semantico/EntradaTabelaDeSimbolos.java
@@ -0,0 +1,33 @@
+package br.ufscar.dc.compiladores.la.semantico;
+
+import java.util.List;
+
+public class EntradaTabelaDeSimbolos {
+
+  String nome;
+  TabelaDeSimbolos.TipoEntradaTds tipoEntrada;
+  TabelaDeSimbolos.TipoVariavel tipoObjeto;
+  List<TabelaDeSimbolos.TipoVariavel> tiposFuncao;
+  TabelaDeSimbolos subTabela;
+
+  public EntradaTabelaDeSimbolos(String nome, TabelaDeSimbolos.TipoEntradaTds tipoEntrada,
+      TabelaDeSimbolos.TipoVariavel tipoObjeto) {
+    this.nome = nome;
+    this.tipoEntrada = tipoEntrada;
+    this.tipoObjeto = tipoObjeto;
+  }
+
+  public EntradaTabelaDeSimbolos(String nome, TabelaDeSimbolos.TipoEntradaTds tipoEntrada,
+      TabelaDeSimbolos.TipoVariavel tipoObjeto, List<TabelaDeSimbolos.TipoVariavel> tiposFuncao) {
+    this.nome = nome;
+    this.tipoEntrada = tipoEntrada;
+    this.tipoObjeto = tipoObjeto;
+    this.tiposFuncao = tiposFuncao;
+  }
+
+  public EntradaTabelaDeSimbolos(TabelaDeSimbolos.TipoEntradaTds tipoEntrada, TabelaDeSimbolos subTabela) {
+    this.tipoEntrada = tipoEntrada;
+    this.subTabela = subTabela;
+  }
+
+}
--- T4/LA/src/main/java/br/ufscar/dc/compiladores/la/semantico/SymbolTableEntry.java
+++
@@ -1,7 +0,0 @@
-package br.ufscar.dc.compiladores.la.semantico;
-
-public class SymbolTableEntry {
-    public String name;
-    public SymbolTable.TypeLAIdentifier identifierType;
-    public SymbolTable.TypeLAVariable variableType;
-}
